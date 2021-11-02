2ac4fc5e5894a3f4413f896ef3375b952807d64c
shruthishruu05 <shruthi.m@perfios.com>
2021-11-02 22:26:50 +0530

    ADDED : Ability to create employeepayrolldata

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.employeepayrollapp.model;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+
+public class EmployeePayrollData {
+	
+	private int employeeId;
+	private String name;
+	private long salary;
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+	public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) {
+		super();
+		this.employeeId = employeeId;
+		this.name = employeePayrollDTO.name;
+		this.salary = employeePayrollDTO.salary;
+	}
+	public EmployeePayrollData() {}
+	
+	
+}
