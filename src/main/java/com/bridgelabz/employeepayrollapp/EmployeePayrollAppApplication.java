8bc88e5200ea711638a6a925b556d8e7ad31ea67
shruthishruu05 <shruthi.m@perfios.com>
2021-11-03 22:39:15 +0530

    ADDED : Ability to set logging the environment from appropriate application property

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -2,6 +2,7 @@ package com.bridgelabz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,8 +12,9 @@ public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
 		
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
-		log.info("Employee Payroll App Started");
+		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
 	}
 
 }
