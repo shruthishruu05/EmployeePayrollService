bfd01cab9e74e9e7905a9725c8101c3a2745c542
shruthishruu05 <shruthi.m@perfios.com>
2021-11-07 16:33:41 +0530

    ADDED : Interface to implement jpa repository

--- src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeePayrollRepository.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeePayrollRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
 
 public interface EmployeePayrollRepository extends JpaRepository<EmployeePayrollData, Integer>{
-	
+	 
 }
