79291029bae9b9797ac497ae6a7cb5726f3cd6d7
shruthishruu05 <shruthi.m@perfios.com>
2021-11-07 15:39:56 +0530

    ADDED : Ability to create table using @entity

--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -2,19 +2,40 @@ package com.bridgelabz.employeepayrollapp.model;
 
 import java.util.List;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 
 import lombok.Data;
 
+@Entity
+@Table(name="employee_payroll")
 public @Data class EmployeePayrollData {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="employee_id")
 	private int employeeId;
+	
+	@Column(name="name")
 	private String name;
 	private long salary;
 	public String gender;
 	public String startDate;
 	public String note;
 	public String profilePic;
+	
+	@ElementCollection
+	@CollectionTable(name="employee_department",joinColumns = @JoinColumn(name="id"))
+	@Column(name="department")
 	public List<String> department;
 	
 	public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) {
