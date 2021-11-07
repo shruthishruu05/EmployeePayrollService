09db83570fe4e5d5ea4d73ab9c7fb57861f0589d
shruthishruu05 <shruthi.m@perfios.com>
2021-11-07 12:44:49 +0530

    ADDED : additional attributes

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import java.util.List;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
-public class EmployeePayrollDTO {
+import lombok.ToString;
+
+public @ToString class EmployeePayrollDTO {
 	
 	@NotEmpty(message="Employee name cannot be null")
 	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message ="Employee name Invalid")
@@ -13,15 +17,10 @@ public class EmployeePayrollDTO {
 	@Min(value=500, message="Min Wage should be more than 500")
 	public long salary;
 	
-	public EmployeePayrollDTO(String name, long salary) {
-		super();
-		this.name = name;
-		this.salary = salary;
-	}
-	@Override
-	public String toString() {
-		return "EmployeePayrollDTO [name=" + name + ", salary=" + salary + "]";
-	}
-	
+	public String gender;
+	public String startDate;
+	public String note;
+	public String profilePic;
+	public List<String> department;
 	
 }
